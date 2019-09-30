290acc54c55cbf4e15d3b8f6c84c8ea2b8fab721
Scott Thiessen <scott@movingobjects.io>
2019-09-29 17:01:53 -0700

    Add base code setup

---
+++ src/Timeline.js
@@ -0,0 +1,10 @@
+
+import * as d3 from 'd3';
+
+export default class Timeline {
+
+  constructor() {
+
+  }
+
+}
---
+++ src/entry.js
@@ -0,0 +1,7 @@
+
+import 'd3-selection-multi';
+
+import style from './styles/style.scss';
+import Timeline from './Timeline.js';
+
+let timeline = new Timeline();
